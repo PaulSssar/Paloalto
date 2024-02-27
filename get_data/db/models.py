1bb331dd882e2a50ab745c2bae9b7b365f227d75
Павел Сарыгин <paulssssar@gmail.com>
2024-02-27 20:48:39 +0700

    исправил версию композ

--- get_data/db/models.py
+++ get_data/db/models.py
@@ -4,7 +4,7 @@ from clickhouse_sqlalchemy import (Table, engines, get_declarative_base,
                                    make_session, types)
 from sqlalchemy import Column, MetaData, create_engine
 
-uri = f'clickhouse://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@clickhouse:8123'
+uri = f'clickhouse://{os.getenv("DB_USER")}@clickhouse:8123'
 
 engine = create_engine(uri, pool_size=20, max_overflow=20)
 session = make_session(engine)
